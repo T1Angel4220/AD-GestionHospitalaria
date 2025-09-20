f48e8a9d8db85cd71a549ed74a17ff5af70f4350
kiramman-mp3 <jrodriguez0612@uta.edu.ec>
2025-09-20 11:47:56 -0500

    Se creo el archivo de servicio para el ORM de prisma

---
+++ hospital-app/backend/src/services/centro.service.ts
@@ -0,0 +1,7 @@
+import { prisma } from '../config/prisma';
+export const list = () => prisma.centroMedico.findMany({ orderBy: { id: 'asc' } });
+export const create = (data: {nombre:string;ciudad:string;direccion?:string|null}) =>
+  prisma.centroMedico.create({ data });
+export const update = (id:number, data: Partial<{nombre:string;ciudad:string;direccion?:string|null}>) =>
+  prisma.centroMedico.update({ where:{ id }, data });
+export const remove = (id:number) => prisma.centroMedico.delete({ where:{ id } });
